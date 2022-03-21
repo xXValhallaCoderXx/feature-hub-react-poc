f4e3061772cfc4a3d2c33b5bafe3dd257dc4f5ef
xXValhallaCoder <renate.gouveia@gmail.com>
2022-03-21 19:17:56 +0800

    Websocket Feature hub working

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import {
+  EdgeFeatureHubConfig,
+  Readyness,
+  FeatureHubPollingClient,
+} from "featurehub-javascript-client-sdk";
+
+let initialized = false;
+let fhConfig;
+let fhClient;
+let fhUrl = "http://localhost:8085";
+let fhAppiKey =
+  "default/7f1cbc2d-0c3f-471d-939e-becd193f89bc/LyEIMrWnTDCxa3h6RuqCOJGDiyxFkXqiBiAzj5AT";
 
 function App() {
+  const [btnColor, setBtnColor] = useState("red");
+  useEffect(() => {
+    (async () => {
+      console.log("INIT APP");
+      fhConfig = new EdgeFeatureHubConfig(fhUrl, fhAppiKey);
+      // fhClient = await fhConfig.newContext().userKey(user).build();
+      fhClient = await fhConfig.newContext().build();
+      fhConfig.addReadynessListener((readyness) => {
+        if (!initialized) {
+          if (readyness === Readyness.Ready) {
+            initialized = true;
+            const color = fhClient.getString("SUBMIT_COLOR_BUTTON");
+            console.log("INIT VALUE - SUBMIT COLOR BUTTON: ", color);
+            setBtnColor(color);
+          }
+        }
+      });
+      fhClient.feature("SUBMIT_COLOR_BUTTON").addListener((fs) => {
+        console.log("UPDATE VALUE - SUBMIT COLOR BUTTON: ", fs.getString());
+        setBtnColor(fs.getString());
+      });
+      return () => {
+        console.log("Tidy Up");
+        fhConfig.close();
+      };
+    })();
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
@@ -9,14 +49,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <button style={{padding: 5, backgroundColor: btnColor}}>Test Buitton</button>
       </header>
     </div>
   );
